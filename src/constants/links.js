6481b3d887cedf74cdf2a7b974f92e2f7cc16fa2
Andrzej <andrzej.michal.kolomanski@gmail.com>
2019-08-31 16:10:10 +0200

    Finished footer and header

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,18 +1,18 @@
 export default [
-    {
-        path: '/',
-        text: 'home',
-    },
-    {
-        path: '/rooms',
-        text: 'rooms',
-    },
-    {
-        path: '/blog',
-        text: 'blog',
-    },
-    {
-        path: '/contact',
-        text: 'contact',
-    },
-]
+	{
+		path: '/',
+		text: 'home'
+	},
+	{
+		path: '/tours',
+		text: 'tours'
+	},
+	{
+		path: '/blog',
+		text: 'blog'
+	},
+	{
+		path: '/contact',
+		text: 'contact'
+	}
+];
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 import {Link} from 'gatsby'
 import Layout from '../components/Layout'
+import SimpleHero from "../components/SimpleHero"
 
 export default () => (
     <>
         <Layout>
-            <div>Hello from backroads</div>
+            <SimpleHero />
         </Layout>
     </>
 )
