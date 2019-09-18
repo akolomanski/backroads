75f00337d0bca2987b4c715a5665432e2a780b78
Andrzej Kolomanski <jedrasek666@gmail.com>
2019-09-18 22:41:41 +0200

    attached to cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,9 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+require('dotenv').config({
+	path: `.env.${process.env.NODE_ENV}`
+});
 
 module.exports = {
 	siteMetadata: {
@@ -14,6 +17,14 @@ module.exports = {
 	plugins: [
 		`gatsby-plugin-sass`,
 		`gatsby-plugin-styled-components`,
+		{
+			resolve: `gatsby-source-contentful`,
+			options: {
+				spaceId: process.env.CONTENTFUL_SPACE_ID,
+				// Learn about environment variables: https://gatsby.dev/env-vars
+				accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+			}
+		},
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
