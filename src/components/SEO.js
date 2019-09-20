60a1a80dce8ebb37942956867e76591ccb35ac33
Andrzej Kolomanski <jedrasek666@gmail.com>
2019-09-20 17:09:29 +0200

    twitter card added

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -10,19 +10,27 @@ const getData = graphql`
 				siteDesc: description
 				author
 				image
-				siteUrl
+                siteUrl
+                twitterUsername
 			}
 		}
 	}
 `;
 
 const SEO = ({ title, description }) => {
-	const { site: { siteMetadata: { siteDesc, siteTitle, author, siteUrl, image } } } = useStaticQuery(getData);
+	const { site: { siteMetadata: { siteDesc, siteTitle, author, siteUrl, image, twitterUsername } } } = useStaticQuery(getData);
 
 	return (
 		<Helmet htmlAttributes={{ lang: 'en' }} title={`${title} | ${siteTitle}`}>
 			<meta name="description" content={description || siteDesc} />
 			<meta name="image" content={image} />
+            {/* twitter card */}
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:creator" content={twitterUsername} />
+            <meta name="twiter:title" content={title} />
+            <meta name="twiter:description" content={siteDesc} />
+            <meta name="twiter:image" content={`${siteUrl}${image}`} />
+            
 		</Helmet>
 	);
 };
